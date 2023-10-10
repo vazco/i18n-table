961d27f2e573bf6a8e3a56b0ca1c90a6e4135e13
Piotr Pośpiech <37746259+piotrpospiech@users.noreply.github.com>
2023-10-10 15:54:26 +0200

    Refactor (#8)

---
+++ src/lib/index.ts
@@ -0,0 +1,5 @@
+export { default as buildClassName } from './buildClassName';
+export { default as getSearchRegex } from './getSearchRegex';
+export { default as initData } from './initData';
+export { default as keyify } from './keyify';
+export * from './constants';
