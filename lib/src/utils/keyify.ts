6cb94a8e886ee61b768a65b7ad7d92febd844444
Piotr Pośpiech <37746259+piotrpospiech@users.noreply.github.com>
2024-01-30 19:56:20 +0100

    Optimized keyify (#15)

--- lib/src/utils/keyify.ts
+++ lib/src/utils/keyify.ts
@@ -1,20 +1,20 @@
 export default function keyify(object: Record<string, unknown>, prefix: unknown[] = []): string[] {
-  return Object.keys(object).reduce<string[]>((result, element) => {
-    if (Array.isArray(object[element])) {
-      return result;
+  const result: string[] = [];
+
+  for (const [element, value] of Object.entries(object)) {
+    if (!Array.isArray(value)) {
+      if (typeof value === "object" && value !== null) {
+        prefix.push(element);
+        const keys = keyify(value as Record<string, unknown>, prefix);
+        prefix.pop();
+
+        result.push(...keys);
+      } else {
+        const finalPrefix = prefix.length ? prefix.join(".") + "." : "";
+        result.push(finalPrefix + element);
+      }
     }
+  }
 
-    if (typeof object[element] === "object" && object[element] !== null) {
-      prefix.push(element);
-      const keys = keyify(object[element] as Record<string, unknown>, prefix);
-      prefix.pop();
-
-      result.push(...keys);
-    } else {
-      const finalPrefix = prefix.length ? prefix.join(".") + "." : "";
-      result.push(finalPrefix + element);
-    }
-
-    return result;
-  }, []);
+  return result;
 }
